53b83de4583942560f2342f74bc22d0d39697625
xinyu-hou <xinyu-hou@qq.com>
2026-02-22 20:23:43 +0800

    feat(app):增加appid参数，优化sse输出，实现nginx一键部署

---
+++ src/main/java/com/miao/ai_gen_web/mapper/AppMapper.java
@@ -0,0 +1,13 @@
+package com.miao.ai_gen_web.mapper;
+
+import com.mybatisflex.core.BaseMapper;
+import com.miao.ai_gen_web.entity.App;
+
+/**
+ * 应用 映射层。
+ *
+ * @author miao
+ */
+public interface AppMapper extends BaseMapper<App> {
+
+}
---
+++ src/main/java/com/miao/ai_gen_web/service/AppService.java
@@ -0,0 +1,29 @@
+package com.miao.ai_gen_web.service;
+
+import com.miao.ai_gen_web.entity.User;
+import com.miao.ai_gen_web.model.dto.app.AppQueryRequest;
+import com.miao.ai_gen_web.model.vo.AppVO;
+import com.mybatisflex.core.query.QueryWrapper;
+import com.mybatisflex.core.service.IService;
+import com.miao.ai_gen_web.entity.App;
+import reactor.core.publisher.Flux;
+
+import java.util.List;
+
+/**
+ * 应用 服务层。
+ *
+ * @author miao
+ */
+public interface AppService extends IService<App> {
+
+    AppVO getAppVO(App app);
+
+    QueryWrapper getQueryWrapper(AppQueryRequest appQueryRequest);
+
+    List<AppVO> getAppVOList(List<App> appList);
+
+    Flux<String> chatToGenCode(Long appId, String message, User loginUser);
+
+    String deployApp(Long appId, User loginUser);
+}
