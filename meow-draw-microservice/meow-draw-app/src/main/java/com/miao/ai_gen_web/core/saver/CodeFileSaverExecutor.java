2127816b0c301b94049388b9dad559d0269042aa
xinyu-hou <xinyu-hou@qq.com>
2026-03-18 11:51:21 +0800

    feat: (微服务app)将paser,saver移入core模块

---
+++ meow-draw-microservice/meow-draw-app/src/main/java/com/miao/ai_gen_web/core/saver/CodeFileSaverExecutor.java
@@ -0,0 +1,37 @@
+package com.miao.ai_gen_web.core.saver;
+
+import com.miao.ai_gen_web.ai.model.HtmlCodeResult;
+import com.miao.ai_gen_web.ai.model.MultiFileCodeResult;
+import com.miao.ai_gen_web.exception.BusinessException;
+import com.miao.ai_gen_web.exception.ErrorCode;
+import com.miao.ai_gen_web.model.enums.CodeGenTypeEnum;
+
+import java.io.File;
+
+/**
+ * 代码文件保存执行器
+ * 根据代码生成类型执行相应的保存逻辑
+ */
+public class CodeFileSaverExecutor {
+
+    private static final HtmlCodeFileSaverTemplate htmlCodeFileSaver = new HtmlCodeFileSaverTemplate();
+
+    private static final MultiFileCodeFileSaverTemplate multiFileCodeFileSaver = new MultiFileCodeFileSaverTemplate();
+
+    /**
+     * 执行代码保存（使用 appId）
+     *
+     * @param codeResult  代码结果对象a
+     * @param codeGenType 代码生成类型
+     * @param appId       应用 ID
+     * @return 保存的目录
+     */
+    public static File executeSaver(Object codeResult, CodeGenTypeEnum codeGenType, Long appId) {
+        return switch (codeGenType) {
+            case HTML -> htmlCodeFileSaver.saveCode((HtmlCodeResult) codeResult, appId);
+            case MULTI_FILE -> multiFileCodeFileSaver.saveCode((MultiFileCodeResult) codeResult, appId);
+            default -> throw new BusinessException(ErrorCode.SYSTEM_ERROR, "不支持的代码生成类型: " + codeGenType);
+        };
+    }
+
+}
