d9c1fc0d7b9e303dc39d348caa5d0a8262a3bc55
xinyu-hou <xinyu-hou@qq.com>
2026-03-18 14:16:43 +0800

    feat: (微服务screenshot)配置Dubbo,启用Dubbo

--- meow-draw-microservice/meow-draw-screenshot/src/main/java/com/miao/ai_gen_web/AiGenWebScreenshotApplication.java
+++ meow-draw-microservice/meow-draw-screenshot/src/main/java/com/miao/ai_gen_web/AiGenWebScreenshotApplication.java
@@ -1,7 +1,11 @@
 package com.miao.ai_gen_web;
 
+import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableDubbo
+@SpringBootApplication
 public class AiGenWebScreenshotApplication {
     public static void main(String[] args) {
         SpringApplication.run(AiGenWebScreenshotApplication.class, args);
---
+++ meow-draw-microservice/meow-draw-screenshot/src/main/java/com/miao/ai_gen_web/service/impl/InnerScreenshotServiceImpl.java
@@ -0,0 +1,20 @@
+package com.miao.ai_gen_web.service.impl;
+
+import com.miao.ai_gen_web.innerservice.InnerScreenshotService;
+import com.miao.ai_gen_web.service.ScreenshotService;
+import jakarta.annotation.Resource;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.dubbo.config.annotation.DubboService;
+
+@DubboService
+@Slf4j
+public class InnerScreenshotServiceImpl implements InnerScreenshotService {
+
+    @Resource
+    private ScreenshotService screenshotService;
+
+    @Override
+    public String generateAndUploadScreenshot(String webUrl) {
+        return screenshotService.generateAndUploadScreenshot(webUrl);
+    }
+}
