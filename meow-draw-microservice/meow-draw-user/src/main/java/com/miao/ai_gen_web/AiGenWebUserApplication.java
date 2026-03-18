8848ca5da64826a10468eeb10d408888d194d6b4
xinyu-hou <xinyu-hou@qq.com>
2026-03-18 12:43:16 +0800

    feat: (微服务user)配置Dubbo,启用Dubbo

--- meow-draw-microservice/meow-draw-user/src/main/java/com/miao/ai_gen_web/AiGenWebUserApplication.java
+++ meow-draw-microservice/meow-draw-user/src/main/java/com/miao/ai_gen_web/AiGenWebUserApplication.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 @MapperScan("com.miao.ai_gen_web.mapper")
 @ComponentScan("com.miao")
+//
 @EnableDubbo
 public class AiGenWebUserApplication {
     public static void main(String[] args) {
