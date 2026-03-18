8763bb6bb19c0159099c46c9657126e7da071bbf
xinyu-hou <xinyu-hou@qq.com>
2026-03-18 11:39:15 +0800

    feat: (微服务screenshot)抽取相关模块到screenshot服务

---
+++ meow-draw-microservice/meow-draw-screenshot/src/main/java/com/miao/ai_gen_web/AiGenWebScreenshotApplication.java
@@ -0,0 +1,9 @@
+package com.miao.ai_gen_web;
+
+import org.springframework.boot.SpringApplication;
+
+public class AiGenWebScreenshotApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AiGenWebScreenshotApplication.class, args);
+    }
+}
